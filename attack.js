0e9ff98351a4ad39edf6ef791cc2430e0e256c79
Matias Acosta <acosta_matias8@hotmail.com>
2022-06-06 17:31:55 -0300

    fix linting

--- attack.js
+++ attack.js
@@ -1,4 +1,2 @@
-export const attack = (creatureName, damage, isCritical) => { 
-    return `${creatureName} dealt ${isCritical ? damage * 2 : damage} damage!`
-}
-
+const attack = (creatureName, damage, isCritical) => `${creatureName} dealt ${isCritical ? damage * 2 : damage} damage!`;
+export default attack;
