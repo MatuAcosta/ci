1c36211bd52887e76152a81de4cea72163b7c6d7
Matias Acosta <acosta_matias8@hotmail.com>
2022-06-06 20:35:43 -0300

    lint test

--- attack.test.js
+++ attack.test.js
@@ -1,9 +1,8 @@
-import attack from "./attack";
+import attack from './attack';
 
-test('should return damage sentence when called correctly', ()=>{
-    const result = attack('Smelly Ogre',23)
-    expect(result).toMatch(/^Smelly Ogre(.*)23/)
-    expect(result).toMatchInlineSnapshot(`"Smelly Ogre dealt 23 damage!"`)
-    //we can use toMatch that allow us to use regex to match a string
-
-})
+test('should return damage sentence when called correctly', () => {
+  const result = attack('Smelly Ogre', 23);
+  expect(result).toMatch(/^Smelly Ogre(.*)23/);
+  expect(result).toMatchInlineSnapshot('"Smelly Ogre dealt 23 damage!"');
+  // we can use toMatch that allow us to use regex to match a string
+});
